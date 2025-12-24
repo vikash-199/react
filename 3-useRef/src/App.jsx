5a412e0e0b660610cd2cafc4a14734c3084d4b04
vikash kumar <vikashkumar24041997@gmail.com>
2025-12-24 23:50:14 +0530

    useMemo imp

--- 3-useRef/src/App.jsx
+++ 3-useRef/src/App.jsx
@@ -1,11 +1,25 @@
-import FocusInput from "./FocusInput";
-import PreviousValue from "./PreviousValue";
+import { useMemo, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  const result = useMemo(() => {
+    let sum = 0;
+    for (let i = 0; i <= 1000000; i++) {
+      sum += i;
+    }
+    return sum + count;
+  }, [count]);
   return (
     <div>
-      {/* <FocusInput /> */}
-      <PreviousValue />
+      <h2>Result:{result}</h2>
+      <button onClick={() => setCount(count + 1)}>Increse Count</button>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
     </div>
   );
 }
